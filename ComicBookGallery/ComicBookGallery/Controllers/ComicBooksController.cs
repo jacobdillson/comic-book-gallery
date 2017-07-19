c2bfe5e86d2e8243c3c6c33c643f2dc79d3af376
jacobdillson <dillson.jacob@gmail.com>
2017-07-19 11:10:06 -0400

    Updated ComicBooks controller to handle a 404 and to use the repository.

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ComicBookGallery.Models;
+using ComicBookGallery.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,9 +12,22 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail() {
+        private ComicBookRepository _comicBookRepository = null;
 
-            return View();
+        public ComicBooksController() {
+            _comicBookRepository = new ComicBookRepository();
         }
+
+        public ActionResult Detail(int? id) {
+
+            if (id == null) {
+                return NotFound();
+            } else {
+                var comicBook = _comicBookRepository.GetComicBook((int)id);
+                return View(comicBook);
+            }
+
+        }
+
     }
 }
