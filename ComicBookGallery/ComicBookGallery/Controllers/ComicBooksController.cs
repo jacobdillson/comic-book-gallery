b55b3d71150cd0f1060b39bc968733a92d37df3a
jacobdillson <dillson.jacob@gmail.com>
2017-07-19 03:15:45 -0400

    Updated Comic Book Detail View with strongly typed models.

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ComicBookGallery.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,21 +13,22 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail() {
 
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description =
-                "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! " +
-                "Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new String[] {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+            var comicBook = new ComicBook() {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml =
+                    "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! " +
+                    "Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[] {
+                    new Artist() {Name = "Dan Slott", Role = "Script"},
+                    new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() {Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() {Name = "Edgar Delgado", Role = "Colors"},
+                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters"}
+                }
             };
 
-
-            return View();
+            return View(comicBook);
         }
     }
 }
